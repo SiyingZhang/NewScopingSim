052c5beeae76f40df9dd5859a3d8ade23a76082f
LexieSu <yus55@pitt.edu>
2016-04-22 21:49:48 -0400

    Yue Su

--- src/main/java/edu/scopingsim/dao/VideoDao.java
+++ src/main/java/edu/scopingsim/dao/VideoDao.java
@@ -1,5 +1,67 @@
 package edu.scopingsim.dao;
 
+/**
+ * Case - Data Access Object
+ * @author siying 
+ */
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+import edu.scopingsim.bean.Case;
+import edu.scopingsim.bean.Video;
+import edu.scopingsim.utils.DatabaseConnector;
+
 public class VideoDao {
+
+		private Connection connection;
+		Statement statement = null;
+		ResultSet rs = null;
+		String query = "";
+
+		public VideoDao() {
+			connection = DatabaseConnector.getConnection();
+			System.out.println("--------- CaseDao Connection --------");
+		}
+		
+		/**
+		 * Insert case record into TABLE case
+		 * @param Case c
+		 * @return case id
+		 */
+		public int insertVideo(Video v, int caseID) {
+			
+			query = "INSERT INTO scopingsim.case(caseName, caseDescription) values (?, ?)"; 
+			//" + c.getCaseId() + "','" + c.getCaseName() + "','" + c.getCaseDescription() + "' )";
+			try {
+				query = "INSERT INTO scopingsim.video(path) values (?)"; 
+				PreparedStatement ps = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+				ps.setString(1, v.getPath());
+				ps.executeUpdate();
+
+				ResultSet rs = ps.getGeneratedKeys();
+				
+				if(rs.next()) {
+					int autoKey = rs.getInt(1);
+					return autoKey;
+				} else {
+					return -1;
+				}
+				
+			} catch (SQLException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				return -1;
+			}
+		}
+		
+		/**
+		 * Select case by case Name
+		 * @param caseName String
+		 * @return selected Case
+		 */
 
 }
